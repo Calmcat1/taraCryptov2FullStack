38fbf948376d75563108015d026f68feeb7b89f1
Lee <123798682+Calmcat1@users.noreply.github.com>
2023-09-07 21:07:00 +0300

    Add files via upload

--- BuisnessAPP(django framework)/buisnessSite/buisnessSite/settings.py
+++ BuisnessAPP(django framework)/buisnessSite/buisnessSite/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
