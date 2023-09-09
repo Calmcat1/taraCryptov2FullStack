24697d158f1f8563e2025f780e911c6ef5d0e091
Lee <123798682+Calmcat1@users.noreply.github.com>
2023-09-09 15:04:00 +0300

    Update admin.py

--- buisnessApp1/admin.py
+++ buisnessApp1/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from .models import blogHeadlineTable, productsTable
 
 
 # Register your models here.
+
+admin.site.register(blogHeadlineTable)
+admin.site.register(productsTable)
 
 
